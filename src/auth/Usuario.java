554fa00305c42e275288d5cba95c947f6e5f7f30
Sérvulo Sávio <servulosavio@gmail.com>
2018-05-09 20:08:25 -0300

    Atualização

--- src/auth/Usuario.java
+++ src/auth/Usuario.java
@@ -8,6 +8,36 @@ public class Usuario {
     
     private String login;
     private String senha;
-    
-    
+    private NivelAcesso nivel_acesso;
+
+    public Usuario(String login, String senha, NivelAcesso nivel_acesso) {
+        this.login = login;
+        this.senha = senha;
+        this.nivel_acesso = nivel_acesso;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public NivelAcesso getNivel_acesso() {
+        return nivel_acesso;
+    }
+
+    public void setNivel_acesso(NivelAcesso nivel_acesso) {
+        this.nivel_acesso = nivel_acesso;
+    }
+      
 }
