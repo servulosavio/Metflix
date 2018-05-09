fe73036ff987be446833e5bdd3f1aed7cb77c2bd
Sérvulo Sávio <servulosavio@gmail.com>
2018-05-09 20:04:07 -0300

    Início

---
+++ src/auth/NivelAcesso.java
@@ -0,0 +1,6 @@
+package auth;
+
+public enum NivelAcesso {
+    GERENTE, ASSINANTE;
+    
+}
---
+++ src/auth/Usuario.java
@@ -0,0 +1,13 @@
+package auth;
+
+/**
+ *
+ * @author servu
+ */
+public class Usuario {
+    
+    private String login;
+    private String senha;
+    
+    
+}
